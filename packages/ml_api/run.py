fef1193ea78404784e8f304a26a4dad1daea45a3
Karthikeyan Sankaran <xyz@gmail.com>
2020-06-24 01:02:57 +0530

    Deploy to Heroku-2

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,7 +1,7 @@
 from api.app import create_app
 from api.config import DevelopmentConfig, ProductionConfig
 
-application = create_app(config_object=DevelopmentConfig)
+application = create_app(config_object=ProductionConfig)
 #application = create_app()
 
 if __name__ == '__main__':
